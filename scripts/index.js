bd3cd46f8aa7bd3058a1fbb2533626dc7322f45e
hotellimabravo <hugobalttazar@gmail.com>
2025-07-05 06:57:35 -0300

    design popup concluido - funcionalidades ok

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+const formElement = document.querySelector('.popup__form');
+
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__close-button');
+
+const nameInput = document.querySelector('#name');
+const descriptionInput = document.querySelector('#description');
+
+const profileName = document.querySelector('.profile__info__name');
+const profileDescription = document.querySelector('.profile__info__title');
+
+function openPopup() {
+	popup.classList.add('popup_opened');
+	nameInput.value = profileName.textContent;
+	descriptionInput.value = profileDescription.textContent;
+}
+
+function closePopup() {
+	popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup);
+
+closeButton.addEventListener('click', closePopup);
+
+function handleProfileFormSubmit(evt) {
+	evt.preventDefault();
+
+	profileName.textContent = nameInput.value;
+	profileDescription.textContent = descriptionInput.value;
+
+	closePopup();
+}
+
+formElement.addEventListener('submit', handleProfileFormSubmit);
