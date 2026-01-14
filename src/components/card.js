f6d4fd41c6cf532243d3351bce658510d09b9853
hotellimabravo <hugobalttazar@gmail.com>
2026-01-14 12:37:45 -0300

    alteração na estrutura para entrega do projeto

--- src/components/card.js
+++ src/components/card.js
@@ -24,22 +24,22 @@ export default class Card {
 
 		const likeBtn = document.createElement('img');
 		likeBtn.classList.add('card__like-button');
-		likeBtn.src = './src/images/heartSVG.svg';
+		likeBtn.src = './images/heartSVG.svg';
 		likeBtn.alt = 'Imagem de um coração para curtidas!';
 
 		likeBtn.addEventListener('click', () => {
 			const isLiked = likeBtn.classList.toggle('liked');
 
 			if (isLiked) {
-				likeBtn.src = './src/images/heartSVGlike.svg';
+				likeBtn.src = './images/heartSVGlike.svg';
 			} else {
-				likeBtn.src = './src/images/heartSVG.svg';
+				likeBtn.src = './images/heartSVG.svg';
 			}
 		});
 
 		const delBtn = document.createElement('img');
 		delBtn.classList.add('card__delete-button');
-		delBtn.src = './src/images/trash.png';
+		delBtn.src = './images/trash.png';
 		delBtn.addEventListener('click', () => {
 			div.remove();
 		});
